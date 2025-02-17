9c332e8ec74c52c17ae8473a8acb1d7cd639621f
neharathod8766 <neharathod8766@gmail.com>
2025-02-17 12:19:57 +0530

    newly deployed

---
+++ Cloud Cuisine Backend/src/main/java/com/app/CloudCuisineApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCuisineApplication {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(CloudCuisineApplication.class, args);
+	}
+}
